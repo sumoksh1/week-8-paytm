fb4f2af42b73f2481cef15eeb5fc76e384aeb0cd
Sumoksh <sumoksh1@gmail.com>
2026-07-10 23:04:29 +0530

    added account schema

--- backend/db.js
+++ backend/db.js
@@ -12,6 +12,21 @@ const userSchema = new mongoose.Schema({
 // Create a model from the schema
 const User = mongoose.model('User', userSchema);
 
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+
 module.exports = {
-	User
+	User,
+    Account
 };
